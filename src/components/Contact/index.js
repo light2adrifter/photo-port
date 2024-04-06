fff9355e39612cdde5a18cf9b7015e70aaf511f7
light <light2adrifter@yahoo.com>
2024-04-06 12:48:11 -0400

    added contact form structure

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+function ContactForm(){
+return (
+    <section>
+        <h1>Contact Me</h1>
+        <form id="contact-form">
+            //name input
+            //email input
+            //message text area
+        </form>
+    </section>
+)
+}
+
+export default ContactForm;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -36,7 +36,7 @@ function Nav(props) {
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
+                currentCategory.name === category.name
                 }`}
               key={category.name}
             >
