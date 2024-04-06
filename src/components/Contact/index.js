d4cf8c6f0e31cd5f983c5b1be1837b833d06c248
light <light2adrifter@yahoo.com>
2024-04-06 13:22:59 -0400

    added contact form 'functionality' for front end

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,14 +1,37 @@
-function ContactForm(){
-return (
-    <section>
-        <h1>Contact Me</h1>
-        <form id="contact-form">
-            //name input
-            //email input
-            //message text area
-        </form>
-    </section>
-)
-}
+import React, { useState } from 'react';
+
+
+function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    <input type="email" name="email" defaultValue={formState.name} />
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    };
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+    ;
+    return (
+        <section>
+            <h1>Contact Me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name"></label>
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email"></label>
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+                </div><div>
+                    <label htmlFor="message"></label>
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
+};
 
 export default ContactForm;
